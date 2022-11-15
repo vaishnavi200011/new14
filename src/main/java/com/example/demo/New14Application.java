14db47aacf5b0d3f6a30a4ce789a4010fdb1ba56
vaishnavi <sablevaishnavi07@gmail.com>
2022-11-15 17:38:59 +0530

    Add successfully

---
+++ src/main/java/com/example/demo/New14Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class New14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(New14Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/New14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class New14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
